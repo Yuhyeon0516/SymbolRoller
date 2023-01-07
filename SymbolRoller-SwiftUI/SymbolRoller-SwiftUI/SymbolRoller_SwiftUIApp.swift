7b0919f59813ff503c38c534e543fb68aaade735
Yuhyeon Kim <120432007+Yuhyeon0516@users.noreply.github.com>
2023-01-07 12:15:17 +0900

    Add files via upload

---
+++ SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRoller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolRoller_SwiftUIApp.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by Yuhyeon Kim on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct SymbolRoller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SymbolRollerView()
+        }
+    }
+}
